4659c5bde1b0d952e4d45e17d1dd8bd7f5178f4e
USER-20160930UC\Administrator <1071858797@qq.com>
2017-07-04 09:30:12 +0800

    evo5.0_SP_01_20170630

---
+++ mysql.sql
@@ -0,0 +1,2 @@
+insert  into `ezmobile_patchinfo`(`patch_Version`,`patch_Name`,`patch_Time`,`patch_editinfo`,`PATCH_EVO`,`isclientuse`) 
+values ('5.0.01','5.0.01_evo_20170630',sysdate(),'WanhuezOFFICE','0','1');
